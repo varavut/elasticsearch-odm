f3093c706778541d5841611e75c284bb9092f425
varavut lormongkol <varavut@outlook.com>
2016-10-16 19:59:18 +0700

    fix findAndUpdate() to support string and complex field.

--- tests/query-and-update.js
+++ tests/query-and-update.js
@@ -13,16 +13,16 @@ describe('Query-And-Update', function () {
     this.timeout(10000)
     helper.connect(app)
       .then(function () {
-        return new Car({ name: 'Ford', speed: 0 }).save()
+        return new Car({ name: 'Ford', speed: 0, owner: {name: 'Arash'} }).save()
       })
       .then(function () {
-        return new Car({ name: 'Chevrolet', speed: 5 }).save()
+        return new Car({ name: 'Chevrolet', speed: 5, owner: {name: 'Diarmuid'} }).save()
       })
       .then(function () {
-        return new Car({ name: 'Honda', speed: 10 }).save()
+        return new Car({ name: 'Honda', speed: 10, owner: {name: 'Siavash'} }).save()
       })
       .then(function () {
-        return new Car({ name: 'Lexus', speed: 15 }).save()
+        return new Car({ name: 'Lexus', speed: 15, owner: {name: 'Kay'} }).save()
       })
       .then(function () {
         // force index refresh
@@ -83,6 +83,41 @@ describe('Query-And-Update', function () {
       Car.find({}).then(function (cars) {
         _.each(cars, (car) => {
           car.speed.should.be.eql(50)
+        })
+        done()
+      }).catch(done)
+    }).catch(done)
+  })
+
+  it('.findAndUpdate() change multiple value', function (done) {
+    Car.findAndUpdate({ name: 'Chevrolet' }, null, { name: 'Ferrari', speed: 100 }).then(function (res) {
+      res.total.should.be.eql(1)
+      res.updated.should.be.eql(1)
+      res.failures.should.be.instanceof(Array)
+      res.failures.should.be.empty()
+      Car.find({name: 'Ferrari'}).then(function (cars) {
+        cars.should.be.instanceof(Array)
+        cars.length.should.be.eql(1)
+        _.each(cars, (car) => {
+          car.speed.should.be.eql(100)
+        })
+        done()
+      }).catch(done)
+    }).catch(done)
+  })
+
+  it('.findAndUpdate() change deep value', function (done) {
+    Car.findAndUpdate({ name: 'Ferrari' }, null, { speed: 150, owner: {name: 'Varavut'} }).then(function (res) {
+      res.total.should.be.eql(1)
+      res.updated.should.be.eql(1)
+      res.failures.should.be.instanceof(Array)
+      res.failures.should.be.empty()
+      Car.find({name: 'Ferrari'}).then(function (cars) {
+        cars.should.be.instanceof(Array)
+        cars.length.should.be.eql(1)
+        _.each(cars, (car) => {
+          car.speed.should.be.eql(150)
+          car.owner.name.should.be.eql('Varavut')
         })
         done()
       }).catch(done)
